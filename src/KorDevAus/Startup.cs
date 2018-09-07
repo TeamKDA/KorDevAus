dc45b1907cb9570250aabcb745af8c9eaca21e55
Justin Yoo <justin.yoojh@gmail.com>
2018-09-08 09:40:10 +1000

    Add URL redirection rule

--- src/KorDevAus/Startup.cs
+++ src/KorDevAus/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -50,6 +46,7 @@ namespace KorDevAus
             {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
+                app.UseRewriter(new RewriteOptions().AddIISUrlRewrite(env.ContentRootFileProvider, "rewrite.xml"));
             }
 
             app.UseHttpsRedirection();
